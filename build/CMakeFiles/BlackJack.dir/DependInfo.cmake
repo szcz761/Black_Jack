46cc812830701634310b8e0d0184fb78ba074f90
szcz761 <szcz761@gmail.com>
2017-11-14 19:15:19 +0100

    z CMake

---
+++ build/CMakeFiles/BlackJack.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/szymon/Pulpit/Black_Jack/src/Carefull_Player.cpp" "/home/szymon/Pulpit/Black_Jack/build/CMakeFiles/BlackJack.dir/src/Carefull_Player.cpp.o"
+  "/home/szymon/Pulpit/Black_Jack/src/Croupier_Player.cpp" "/home/szymon/Pulpit/Black_Jack/build/CMakeFiles/BlackJack.dir/src/Croupier_Player.cpp.o"
+  "/home/szymon/Pulpit/Black_Jack/src/Game.cpp" "/home/szymon/Pulpit/Black_Jack/build/CMakeFiles/BlackJack.dir/src/Game.cpp.o"
+  "/home/szymon/Pulpit/Black_Jack/src/My_Player.cpp" "/home/szymon/Pulpit/Black_Jack/build/CMakeFiles/BlackJack.dir/src/My_Player.cpp.o"
+  "/home/szymon/Pulpit/Black_Jack/src/Player.cpp" "/home/szymon/Pulpit/Black_Jack/build/CMakeFiles/BlackJack.dir/src/Player.cpp.o"
+  "/home/szymon/Pulpit/Black_Jack/src/main.cpp" "/home/szymon/Pulpit/Black_Jack/build/CMakeFiles/BlackJack.dir/src/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../inc"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CMakeFiles/BlackJack.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/BlackJack.dir/src/My_Player.cpp.o"
+  "CMakeFiles/BlackJack.dir/src/Croupier_Player.cpp.o"
+  "CMakeFiles/BlackJack.dir/src/Carefull_Player.cpp.o"
+  "CMakeFiles/BlackJack.dir/src/Player.cpp.o"
+  "CMakeFiles/BlackJack.dir/src/Game.cpp.o"
+  "CMakeFiles/BlackJack.dir/src/main.cpp.o"
+  "BlackJack.pdb"
+  "BlackJack"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/BlackJack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
